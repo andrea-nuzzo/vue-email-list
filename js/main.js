e8f4ab69e56f7e21127e4b025bc354e80d839b0d
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-12-09 17:21:10 +0100

    aggiunti commenti js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
-/*Attrvaerso l'apposita API di Boolean :api:
-
-generare 10 indirizzi email e stamparli in pagina all'interno di una lista
-Bonus
-Far comparire gli indirizzi email solamente quando sono stati tutti generati. (modificato) */
-
 const app = new Vue({
-
     el:"#root",
 
     data:{
+        //Dichiaro un array vuoto dove inserire le mail richiamate dal server
         mails:[],
     },
 
 
     methods:{
+        // Questa funzione controlla il numero di mail (Bonus) e se sono maggiori di dieci le mostro a schermo
         numbMails: function(){
            if (this.mails.length >= 10){
                return true;
@@ -23,13 +18,14 @@ const app = new Vue({
     },
 
     created() {
+        // Ciclo per ottenere dal server 10 mail differenti
         for(let i = 1; i <= 10; i++){
-
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((response) => {
+
+                // Pusho le mail all'interno dei data
                 this.mails.push(response.data.response);
             })    
-
             .catch(function (error) {
                 // handle error
                 console.log(error);
