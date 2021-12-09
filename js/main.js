275a75ad15f32460a15259f3ea29b4ac0b4d2892
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-12-09 15:19:06 +0100

    struttura base

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/*Attrvaerso l'apposita API di Boolean :api:
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati. (modificato) */
+
+const app = new Vue({
+
+    el:"#root",
+    data:{},
+    methods:{},
+    
+});
