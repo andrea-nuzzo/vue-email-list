4861b15431830a14ef130e2d8976b3ac85fbed1d
Andrea <57769818+andrea-nuzzo@users.noreply.github.com>
2021-12-09 17:17:43 +0100

    esercizio completato

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*Attrvaerso l'apposita API di Boolean :api:
-https://flynn.boolean.careers/exercises/api/random/mail
+
 generare 10 indirizzi email e stamparli in pagina all'interno di una lista
 Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati. (modificato) */
@@ -7,7 +7,35 @@ Far comparire gli indirizzi email solamente quando sono stati tutti generati. (m
 const app = new Vue({
 
     el:"#root",
-    data:{},
-    methods:{},
+
+    data:{
+        mails:[],
+    },
+
+
+    methods:{
+        numbMails: function(){
+           if (this.mails.length >= 10){
+               return true;
+           } else{
+               return false;}
+        }
+    },
+
+    created() {
+        for(let i = 1; i <= 10; i++){
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                this.mails.push(response.data.response);
+            })    
+
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+        } 
+    },
+
     
 });
